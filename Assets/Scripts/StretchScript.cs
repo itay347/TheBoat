da2adb572f2bb88f57749385d1a3d7e983ee3dd7
Itay <itay647@gmail.com>
2017-07-04 19:28:54 +0300

    Add stretch script and setup basic structure
    + Add script to the camera (the game now stretches to fit screen size)
    + Add background and boat
    - Remove android tv compatibility

---
+++ Assets/Scripts/StretchScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StretchScript : MonoBehaviour {
+
+    public float orthographicSize = 4;
+    public float aspect = 0.6f;
+
+    void Start()
+    {
+        Camera.main.projectionMatrix = Matrix4x4.Ortho(
+                -orthographicSize * aspect, orthographicSize * aspect,
+                -orthographicSize, orthographicSize,
+                Camera.main.nearClipPlane, Camera.main.farClipPlane);
+    }
+
+}
